bfe5bdd3c9bb1904ddad9b63d6b11adcaa4998f7
InmobiliariaPolyline <promotorinmobiliario2021@gmail.com>
2026-08-22 21:02:53 -0500

    fix: restore generated prisma client import path

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import helmet from 'helmet';
 import dotenv from 'dotenv';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './generated/prisma/client';
-
 dotenv.config();
 
 const adapter = new PrismaPg({
